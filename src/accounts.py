5b9e85dee4b5e10ad7b23346bffe524eecd6650c
calvinpete <tinkacalvin@gmail.com>
2018-09-19 19:04:33 +0300

    [Delivers #160621162] create accounts.py for managing accounts

---
+++ src/accounts.py
@@ -0,0 +1,29 @@
+# This file contains code for managing an account
+
+accounts = {}  # dictionary where key is the password and value is User
+
+
+def add_account(name, password):
+    """
+    This function adds the Key value pair to the accounts
+    :param name:
+    :param password:
+    :return:
+    """
+    accounts[password] = name
+    return accounts
+
+
+def login(name, password):
+    """
+    This function returns true if the password and corresponding name exist in the accounts dictionary
+    or else add them to the accounts dictionary
+    :param name:
+    :param password:
+    :return: True
+    """
+    for key, value in accounts.items():
+        if password == key and name == value:
+            return True
+    else:
+        add_account(name, password)
