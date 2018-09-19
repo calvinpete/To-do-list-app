7de5da548dea945c3a706dca5b67ad7569ff90bc
calvinpete <tinkacalvin@gmail.com>
2018-09-20 00:31:07 +0300

    [Delivers #160621451] add input validation to the home_page and add functionality to the Sign up option

--- src/app.py
+++ src/app.py
@@ -9,3 +9,29 @@ if __name__ == "__main__":
 
     dashboard = '\nSelect Option:\n1: Create Task\n2: Delete Task\n3: Delete all Tasks\n4: Mark a Task as finished\n' \
                 '5. Back to the home page\n'
+
+    exit_app = False
+
+    while not exit_app:
+        # Allows a user to choose an option on the home_page
+        print(home_page)
+        home_selection = int(input("selection: "))
+        # Checks if the option selected exists
+        if 0 <= home_selection >= 4:
+            print ("Option does not exist, please try again")
+            continue
+        else:
+            # checks if the input is an integer
+            while type(home_selection) is int:
+                # Allows a user to sign up
+                if home_selection is 1:
+                    name = str(input("Enter your name: "))  # Allows a user to enter his/her name
+                    password = str(input("Enter your password: "))  # Allows a user to enter his/her password
+                    if len(name) + len(password) != 0:
+                        add_account(name, password)  # creates an account for the user
+                        # Takes the user back to the home_page
+                        print(home_page)
+                        home_selection = int(input("selection: "))
+                    else:
+                        print("Invalid entry, please try again")
+                        continue
