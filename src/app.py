b38491b33d727804997fdd18c493174a99efffd3
calvinpete <tinkacalvin@gmail.com>
2018-09-20 00:01:19 +0300

    [Delivers #160621451] create a home_page on a console

--- src/app.py
+++ src/app.py
@@ -5,65 +5,38 @@ from tasks import *
 
 if __name__ == "__main__":
 
-    home_page = 'Welcome to the To-do list\n1. Sign up\n2. Login\n3. Exit'
+    home_page = '\nWelcome to the To-do list\n1. Sign up\n2. Login\n3. Exit\n'
 
-    dashboard = 'Select Option:\n1: Create Task\n2: Delete Task\n3: Delete all Tasks\n4: Mark a Task as finished'
+    dashboard = '\nSelect Option:\n1: Create Task\n2: Delete Task\n3: Delete all Tasks\n4: Mark a Task as finished\n' \
+                '5. Back to the home page\n'
 
-    Exit = False
+    exit_app = False
 
-    while not Exit:
+    while not exit_app:
+        # Allows a user to choose an option on the home_page
         print(home_page)
         home_selection = int(input("selection: "))
-        # Allows a user to sign up
-        while home_selection.isdigit():
-            if home_selection is 1:
-                name = input("Enter your name: ")  # Allows a user to enter his/her name
-                password = input("Enter your password: ")  # Allows a user to enter his/her password
-                if add_account(name, password):
-                    print(home_page)
-                    home_selection = int(input("selection: "))
-                else:
-                    print("No entry made, please try again")
-            # Allows a user to login
-            if home_selection is 2:
-                name = input("Enter your name: ")  # Allows a user to enter his/her name
-                password = input("Enter your password: ")  # Allows a user to enter his/her password
-                if login(name, password):# Allows the user to sign
-                    print(dashboard)
-                    selection = int(input("selection: "))
-                    while selection.isdigit():
-                        if selection is 1:
-                            task = str(input("Enter a task to add: "))
-                            if create_task(task):
-                                print("Task added")
-                            else:
-                                print("Invalid entry")
-                                task = str(input("Enter a task to add: "))
-                        if selection is 2:
-                            task = str(input("Enter the task to delete: "))
-                            if delete_task(task):
-                                print ("Task deleted")
-                            else:
-                                print("Invalid entry")
-                                task = str(input("Enter a task to add: "))
-                        if selection is 3:
-                            if delete_all_tasks():
-                                print("All tasks deleted")
-                                print(dashboard)
-                                selection = int(input("selection: "))
-                            else:
-                                print("Error occurred")
-                                print(dashboard)
-                                selection = int(input("selection: "))
-                        if selection is 4:
-                            task = str(input("Enter the finished task: "))
-                            if mark_as_finished(task):
-                                print("Task successfully marked")
-                                print(todo_list)
-                            else:
-                                print("Invalid entry")
-                                task = str(input("Enter the finished task: "))
-            if
+        # Checks if the option selected exists
+        if 0 <= home_selection >= 4:
+            print ("Option does not exist, please try again")
+            continue
         else:
-            print("Invalid option, please try again")
-            home_selection = int(input("selection: "))
+            # checks if the input is an integer
+            while type(home_selection) is int:
+                # Allows a user to sign up
+                if home_selection is 1:
+                    name = str(input("Enter your name: "))  # Allows a user to enter his/her name
+                    password = str(input("Enter your password: "))  # Allows a user to enter his/her password
+                    if len(name) + len(password) != 0:
+                        add_account(name, password)  # creates an account for the user
+                        # Allows the user navigate back to the home_page
+                        print(home_page)
+                        home_selection = int(input("selection: "))
+                    else:
+                        print("Invalid entry, please try again")
+                        continue
+                # Allows the user to exit the application
+                if home_selection is 3:
+                    print ("Thank you for using the To_do list app.")
+                    exit_app = True
+                    break
