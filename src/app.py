a08a4272f1266db46026d401aca9f7410fe55346
calvinpete <tinkacalvin@gmail.com>
2018-09-20 00:24:56 +0300

    [Delivers #160621451] add home_page and dashboard variables for easy user navigation

---
+++ src/app.py
@@ -0,0 +1,11 @@
+# This file contains the code that acts as entry point for a user to use the Todo_list app
+
+from accounts import *
+from tasks import *
+
+if __name__ == "__main__":
+
+    home_page = '\nWelcome to the To-do list\n1. Sign up\n2. Login\n3. Exit\n'
+
+    dashboard = '\nSelect Option:\n1: Create Task\n2: Delete Task\n3: Delete all Tasks\n4: Mark a Task as finished\n' \
+                '5. Back to the home page\n'
