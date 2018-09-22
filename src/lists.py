faea3955e38d576be215c7d1467b6132f4b17b49
calvinpete <tinkacalvin@gmail.com>
2018-09-22 16:29:34 +0300

    [Delivers #160704393] build create_list function

--- src/lists.py
+++ src/lists.py
@@ -4,3 +4,12 @@ from src import tasks
 
 record = {}
 
+
+def create_list(title):
+    """
+    This creates a todo_list with a title in a record of todo_lists
+    :param title:
+    :return: record
+    """
+    record[title] = tasks.todo_list
+    return record
