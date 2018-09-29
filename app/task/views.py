3a4be793f3483413f11bd957ea1f0548459f9583
calvinpete <tinkacalvin@gmail.com>
2018-09-29 18:59:31 +0300

    [Delivers #160870844] add error handlers

--- app/task/views.py
+++ app/task/views.py
@@ -5,11 +5,31 @@ from app.tasks import Tasks
 task = Tasks()
 
 
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({"message": "Invalid entry"}), 400
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({"message": "This page does not exist"}), 404
+
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return jsonify({"message": "This method is not allowed for the requested URL"}), 405
+
+
 @app.route('/todo/api/v1/tasks', methods=['POST'])
 def create_to_do_list():
+
+    if "title" not in request.json:
+        return jsonify({"message": "Invalid entry, please type the title"}), 400
+
     data = request.get_json()
     title = data['title']
-
+    if isinstance(title, int) or isinstance(title, float) or isinstance(title, list):
+        return jsonify({"message": "Please enter a string"})
     if not title:
         return jsonify({"message": "Title field is empty"}), 400
     if title.isspace():
