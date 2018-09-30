0e62381d2db57153691e6b296570482186d28278
calvinpete <tinkacalvin@gmail.com>
2018-09-30 13:42:09 +0300

    [Delivers #160843048] create add task route

--- app/task/views.py
+++ app/task/views.py
@@ -2,7 +2,7 @@ from flask import jsonify, request
 from app import app
 from app.tasks import Tasks
 
-task = Tasks()
+new_tasks = Tasks()
 
 
 @app.errorhandler(400)
@@ -34,9 +34,26 @@ def create_to_do_list():
         return jsonify({"message": "Title field is empty"}), 400
     if title.isspace():
         return jsonify({"message": "Title field is empty"}), 400
-    if task.check_list(title):
+    if new_tasks.check_list(title):
         return jsonify({"message": "To do list already exists"}), 409
-    task.new_list(title)
+    new_tasks.new_list(title)
     return jsonify({"message": "To do list created successfully"}), 201
 
 
+@app.route('/todo/api/v1/tasks/<title>', methods=['POST'])
+def add_task(title):
+    if "task" not in request.json:
+        return jsonify({"message": "Invalid entry, please type the task"}), 400
+    data = request.get_json()
+    task = data["task"]
+    if isinstance(task, int) or isinstance(task, float) or isinstance(task, list):
+        return jsonify({"message": "Please enter a string"}), 400
+    if not task:
+        return jsonify({"message": "Task field is empty"}), 400
+    if task.isspace():
+        return jsonify({"message": "Task field is empty"}), 400
+    if title in new_tasks.record:
+        new_tasks.add_task(title, task)
+        return jsonify({"message": "Task successfully added"}), 201
+    else:
+        return jsonify({"message": "To do list does not exist"}), 404
