ba93c439691b281d01f52584153398149a0de2b6
calvinpete <tinkacalvin@gmail.com>
2018-10-02 23:52:12 +0300

    [Delivers #160843048] test add task route

--- tests/test_tasks_endpoints.py
+++ tests/test_tasks_endpoints.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from tests.test_base import TestBase
 
 
@@ -6,4 +7,14 @@ class TestTaskApi(TestBase):
     """
     This is a class that runs unittests on the tasks api endpoints
     """
-    pass
+    def test_add_task(self):
+        """
+        This tests post a new task route
+        """
+        self.app.post("/todo/api/v1/tasks", content_type="application/json",
+                      data=json.dumps(self.test_data18))
+        response = self.app.post('/todo/api/v1/tasks/Day 1', content_type="application/json",
+                                 data=json.dumps(self.test_data2))
+        self.assertTrue(response.status_code, 201)
+        response_message = json.loads(response.data.decode())
+        self.assertIn("Task successfully added", response_message["message"])
