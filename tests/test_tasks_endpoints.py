6873cae8b772dc2b6bc644ce1cb42f3d76a309fb
calvinpete <tinkacalvin@gmail.com>
2018-10-03 00:03:53 +0300

    [Delivers #160843048] test no entry

--- tests/test_tasks_endpoints.py
+++ tests/test_tasks_endpoints.py
@@ -1,4 +1,3 @@
-import unittest
 import json
 from tests.test_base import TestBase
 
@@ -7,6 +6,7 @@ class TestTaskApi(TestBase):
     """
     This is a class that runs unittests on the tasks api endpoints
     """
+
     def test_add_task(self):
         """
         This tests post a new task route
@@ -18,3 +18,13 @@ class TestTaskApi(TestBase):
         self.assertTrue(response.status_code, 201)
         response_message = json.loads(response.data.decode())
         self.assertIn("Task successfully added", response_message["message"])
+
+    def test_no_entry(self):
+        """This tests a post method with no task field"""
+        self.app.post("/todo/api/v1/tasks", content_type="application/json",
+                      data=json.dumps(self.test_data18))
+        response = self.app.post('/todo/api/v1/tasks/Day 1', content_type="application/json",
+                                 data=json.dumps(self.test_data21))
+        self.assertTrue(response.status_code, 400)
+        response_message = json.loads(response.data.decode())
+        self.assertIn("Invalid entry, please type the task", response_message["message"])
