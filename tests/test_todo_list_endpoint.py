8151c8dd2efb3cf8221b9580dceae736d455e0bc
calvinpete <tinkacalvin@gmail.com>
2018-10-02 23:41:03 +0300

    [Delivers #160870502] test no value entry

--- tests/test_todo_list_endpoint.py
+++ tests/test_todo_list_endpoint.py
@@ -60,6 +60,16 @@ class TestListApi(TestBase):
         response_message = json.loads(response.data.decode())
         self.assertIn("Please enter a string", response_message["message"])
 
+    def test_no_value_entry(self):
+        """
+        This tests a post method with no value in the key/value pair entry
+        """
+        response = self.app.post("/todo/api/v1/tasks", content_type="application/json",
+                                 data=json.dumps(self.test_data15))
+        self.assertEqual(response.status_code, 400)
+        response_message = json.loads(response.data.decode())
+        self.assertIn("Title field is empty", response_message["message"])
+
     def test_whitespace_entry(self):
         """
         This tests a post method with a whitespace invalid entry
