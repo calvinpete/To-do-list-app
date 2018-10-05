9fe1376af44409b1e82796fef688e9f1ae8f862d
calvinpete <tinkacalvin@gmail.com>
2018-10-05 13:12:52 +0300

    [Delivers #160849107] build accounts model testsuite

---
+++ tests/test_accounts.py
@@ -0,0 +1,8 @@
+import unittest
+from app.accounts import Account
+from tests.test_base import TestBase
+
+
+class AccountTestcase(TestBase):
+    """This class holds the tests on the accounts model"""
+    pass
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,7 @@
 import unittest
 from instance.config import app_config
 from app.task.views import *
+from app.accounts import Account
 
 
 class TestBase(unittest.TestCase):
@@ -47,6 +48,28 @@ class TestBase(unittest.TestCase):
         self.test_data81 = {"task": "Reviews"}
         self.test_data82 = {"task": "Updates"}
         self.test_data83 = {"task": "Updates"}
+        self.user_account = Account()
+        self.test_user1 = {"username": "JohnP", "email_address": "JohnP@gmail.com", "password": "Hal0-pEt7&"}
+        self.test_user11 = {}
+        self.test_user12 = {"username": 5, "email_address": "JohnP@gmail.com", "password": "Hal0-pEt7&"}
+        self.test_user13 = {"username": 5.5, "email_address": "JohnP@gmail.com", "password": "Hal0-pEt7&"}
+        self.test_user14 = {"username": ["JohnP"], "email_address": "JohnP@gmail.com", "password": "Hal0-pEt7&"}
+        self.test_user15 = {"username": "JohnP", "email_address": 6, "password": "Hal0-pEt7&"}
+        self.test_user16 = {"username": "JohnP", "email_address": 6.8, "password": "Hal0-pEt7&"}
+        self.test_user17 = {"username": "JohnP", "email_address": ["JohnP@gmail.com"], "password": "Hal0-pEt7&"}
+        self.test_user18 = {"username": "JohnP", "email_address": "JohnP@gmail.com", "password": 7}
+        self.test_user19 = {"username": "JohnP", "email_address": "JohnP@gmail.com", "password": 7.9}
+        self.test_user20 = {"username": "JohnP", "email_address": "JohnP@gmail.com", "password": ['hW?5', 8]}
+        self.test_user21 = {"username": "JohnP", "email_address": "JohnP@gmail.com", "password": "HALWWWWWWW"}
+        self.test_user22 = {"username": "RonaldMark", "email_address": "markronald.com", "password": "marky-6male"}
+        self.test_user23 = {"username": "Lh", "email_address": "Jemah8lo@hotmail.com", "password": "j9W>olgm"}
+        self.test_user24 = {"username": "KingDavid", "email_address": "davidking@gmail.com", "password": "psaLms198?"}
+        self.test_user25 = {"username": "MWFGCBH", "email_address": "MH@gmail.com", "password": "suQ3?kdright"}
+        self.test_user26 = {"username": "YossiFunke", "email_address": "tinkacalvin@gmail.com", "password": "Rejo78!ce"}
+        self.test_user27 = {"username": "JohnP", "email_address": "JohnP@gmail.com", "password": "Hal0-pEt7&"}
+        self.test_user28 = {"email_address": "JohnP@gmail.com", "password": "Hal0-pEt7&"}
+        self.test_user29 = {"username": "JohnP", "password": "Hal0-pEt7&"}
+        self.test_user30 = {"username": "JohnP", "email_address": "JohnP@gmail.com"}
 
     def test_existence(self):
         """
