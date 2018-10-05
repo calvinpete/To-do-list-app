83a1f52cebf72fc346ad5042ee5e85c588ab00de
calvinpete <tinkacalvin@gmail.com>
2018-10-05 18:34:56 +0300

    [Delivers #160842925] test register route

--- tests/test_accounts_endpoints.py
+++ tests/test_accounts_endpoints.py
@@ -1,3 +1,21 @@
 import unittest
 import json
 from tests.test_base import TestBase
+
+
+class AccountTestCase(TestBase):
+    """
+    This class holds the unittests on the login and signup API endpoints
+    """
+    def test_register(self):
+        """This tests the register route"""
+        response = self.app.post('/todo/api/v1/auth/register', content_type="application/json",
+                                 data=json.dumps(self.test_user26))
+        self.assertTrue(response.status_code, 201)
+        response_message = json.loads(response.data.decode())
+        self.assertIn("You've been successfully registered", response_message["message"])
+
+
+
+
+
