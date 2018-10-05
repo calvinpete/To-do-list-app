7708d24a0ac928d330a6f0d3644d1c322491822f
calvinpete <tinkacalvin@gmail.com>
2018-10-05 18:35:55 +0300

    [Delivers #160842925] test nonexistent fields

--- tests/test_accounts_endpoints.py
+++ tests/test_accounts_endpoints.py
@@ -15,7 +15,29 @@ class AccountTestCase(TestBase):
         response_message = json.loads(response.data.decode())
         self.assertIn("You've been successfully registered", response_message["message"])
 
+    def test_nonexistent_username(self):
+        """This tests a post method with no username field"""
+        response = self.app.post('/todo/api/v1/auth/register', content_type="application/json",
+                                 data=json.dumps(self.test_user28))
+        self.assertTrue(response.status_code, 400)
+        response_message = json.loads(response.data.decode())
+        self.assertIn("please type in the username, email address and password", response_message["message"])
 
+    def test_nonexistent_email_address(self):
+        """This tests a post method with no email_address field"""
+        response = self.app.post('/todo/api/v1/auth/register', content_type="application/json",
+                                 data=json.dumps(self.test_user29))
+        self.assertTrue(response.status_code, 400)
+        response_message = json.loads(response.data.decode())
+        self.assertIn("please type in the username, email address and password", response_message["message"])
+
+    def test_nonexistent_password(self):
+        """This tests a post method with no password field"""
+        response = self.app.post('/todo/api/v1/auth/register', content_type="application/json",
+                                 data=json.dumps(self.test_user30))
+        self.assertTrue(response.status_code, 400)
+        response_message = json.loads(response.data.decode())
+        self.assertIn("please type in the username, email address and password", response_message["message"])
 
 
 
