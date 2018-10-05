bb0fa9f00054ee9d383f49a989c585df20a5933e
calvinpete <tinkacalvin@gmail.com>
2018-10-05 13:09:39 +0300

    [Delivers #160870844] remove dead code

--- app/account/views.py
+++ app/account/views.py
@@ -9,21 +9,6 @@ from functools import wraps
 new_account = Account()
 
 
-@app.errorhandler(400)
-def bad_request(error):
-    return jsonify({"message": "Invalid entry"})
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return jsonify({"message": "This page does not exist"})
-
-
-@app.errorhandler(405)
-def method_not_allowed(error):
-    return jsonify({"message": "This method is not allowed for the requested URL"})
-
-
 @app.route('/todo/api/v1/auth/register', methods=['POST'])
 def register():
     if 'username' not in request.json or 'email_address' not in request.json or 'password' not in request.json:
